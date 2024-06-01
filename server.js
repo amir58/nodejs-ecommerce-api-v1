aff283e8d550f7f8bc42ed59297bc6df4d7b1452
AmirMohammed <devamir11@gmail.com>
2024-06-01 22:59:21 +0300

    Add ApiError class and update error handling middleware
    This commit adds a new class called ApiError to handle operation errors that can be predicted. It also updates the error handling middleware to use this class and provide more detailed error responses, including the error status, message, data, and stack trace.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 
 dotenv.config({ path: "config.env" });
 
+const ApiError = require("./utils/apiError");
 const dbConnection = require("./config/database");
 const categoryRoute = require("./routes/categoryRoute");
 
@@ -23,16 +24,24 @@ if (process.env.MODE_ENV === "development") {
 // Routes
 app.use("/api/v1/categories", categoryRoute);
 
+// Run when call route not found
 app.all("*", (req, res, next) => {
   // Create error and send it to error handling middleware
-  const error = new Error(`Cant find ${req.originalUrl} on this server`);
-  next(error.message);
+  // const error = new Error(`Cant find ${req.originalUrl} on this server`);
+  next(new ApiError(`Cant find ${req.originalUrl} on this server`, 404));
 });
 
 // Global error handling middleware
 app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
   res.status(404).json({
-    message: err,
+    status: err.status,
+    message: err.message,
+    data: null,
+    error: err,
+    stack: err.stack,
   });
 });
 
