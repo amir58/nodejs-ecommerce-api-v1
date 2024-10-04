c704851dd1c7b5548001c1098d7f1b3f63ef9f72
AmirMohammed <a.mohamed@exab.sa>
2024-10-04 17:38:53 +0300

    Refactor category model and add image URL generation
    - Updated import statement for mongoose to use template literals for consistency.
    - Modified schema validation messages and constraints to use template literals for consistency.
    - Introduced a new function `setImageURL` to generate the full image URL based on the environment variable `BASE_URL`.
    - Added post-save and post-init hooks to the category schema to automatically update the image URL when a document is saved or retrieved.

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require( "mongoose" );
 
 // 1 - Create schema
 const categorySchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Category required"],
-      unique: [true, "Category must be unique"],
-      minLength: [3, "Too short category name"],
-      maxLength: [32, "Too long category name"],
+      required: [ true, "Category required" ],
+      unique: [ true, "Category must be unique" ],
+      minLength: [ 3, "Too short category name" ],
+      maxLength: [ 32, "Too long category name" ],
     },
     slug: {
       type: String,
@@ -19,7 +19,18 @@ const categorySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+const setImageURL = ( doc ) => {
+  if ( doc.image ) {
+    const imageUrl = `${ process.env.BASE_URL }/categories/${ doc.image }`;
+    doc.image = imageUrl;
+  }
+}
+
+categorySchema.post( 'init', ( doc ) => { setImageURL( doc ) } );
+categorySchema.post( 'save', ( doc ) => { setImageURL( doc ) } );
+
+
 // 2 - Create model
-const categoryModel = mongoose.model("Category", categorySchema);
+const categoryModel = mongoose.model( "Category", categorySchema );
 
 module.exports = categoryModel;
