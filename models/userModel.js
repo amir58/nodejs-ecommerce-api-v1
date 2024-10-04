7256b31422b3835cf87d2823f34cf1fa1a3bafa5
AmirMohammed <a.mohamed@exab.sa>
2024-10-04 23:32:42 +0300

    Add bcrypt for password hashing in user model
    This commit introduces the bcrypt library to the project, enabling secure password hashing within the user model. The userSchema now includes a pre-save middleware that hashes the user's password before saving it to the database. This enhancement improves the security of user data by ensuring that passwords are not stored in plain text.
    
    Additionally, the package-lock.json file has been updated to reflect the new dependency and its associated transitive dependencies. This includes updates to various packages such as @mapbox/node-pre-gyp, agent-base, node-fetch, and others, which are required for the proper functioning of bcrypt.
    
    The package.json file has also been modified to include the bcrypt dependency.

--- models/userModel.js
+++ models/userModel.js
@@ -1,43 +1,52 @@
-const mongoose = require('mongoose');
+const bcrypt = require( 'bcrypt' );
+const mongoose = require( 'mongoose' );
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: [true, 'name required'],
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: [ true, 'name required' ],
+        },
+        slug: {
+            type: String,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: [ true, 'email required' ],
+            unique: true,
+            lowercase: true,
+        },
+        phone: String,
+        profileImage: String,
+        password: {
+            type: String,
+            required: [ true, 'password required' ],
+            minlength: [ 6, 'Too short password' ],
+        },
+        role: {
+            type: String,
+            enum: [ 'user', 'manager', 'admin' ],
+            default: 'user',
+        },
+        active: {
+            type: Boolean,
+            default: true,
+        },
     },
-    slug: {
-      type: String,
-      lowercase: true,
-    },
-    email: {
-      type: String,
-      required: [true, 'email required'],
-      unique: true,
-      lowercase: true,
-    },
-    phone: String,
-    profileImage: String,
-    password: {
-      type: String,
-      required: [true, 'password required'],
-      minlength: [6, 'Too short password'],
-    },
-    role: {
-      type: String,
-      enum: ['user', 'manager', 'admin'],
-      default: 'user',
-    },
-    active: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
+userSchema.pre( 'save', async function ( next ) {
+    if ( !this.isModified( 'password' ) ) {
+        return next();
+    }
 
-const User = mongoose.model('User', userSchema);
+    this.password = await bcrypt.hashSync( this.password, 12 );
+    next();
+} );
+
+const User = mongoose.model( 'User', userSchema );
 
 module.exports = User;
