b452efc0f0ea76f36c0cb853210f8978be0628e0
AmirMohammed <a.mohamed@exab.sa>
2024-10-07 23:48:56 +0300

    Add Review Model Schema
    Create a new Mongoose schema for the Review model, defining fields such as `comment`, `rating`, `product`, and `user`. The `rating` field includes validation to ensure it falls within the range of 1 to 5. The schema also includes timestamps for automatic creation and update tracking. Export the model as `Review` for use in the application.

---
+++ models/reviewModel.js
@@ -0,0 +1,32 @@
+const mongoose = require( "mongoose" );
+
+// 1 - Create schema
+const reviewSchema = new mongoose.Schema(
+    {
+        comment: {
+            type: String,
+            // required: [ true, "Review must have a comment" ],
+        },
+        rating: {
+            type: Number,
+            min: [ 1, "Rating must be at least 1" ],
+            max: [ 5, "Rating must be at most 5" ],
+            required: [ true, "Review must have a rating" ],
+        },
+        product: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Product",
+            required: [ true, "Review must belong to a product" ],
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            required: [ true, "Review must belong to a user" ],
+        },
+    },
+    { timestamps: true }
+);
+
+const reviewModel = mongoose.model( "Review", reviewSchema );
+
+module.exports = reviewModel;
