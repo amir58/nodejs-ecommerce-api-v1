99273f830a99e7d333253358569ee6e4328b67ee
AmirMohammed <a.mohamed@exab.sa>
2024-10-25 01:12:19 +0300

    Refactor Review Model and Validators
    - Added a static method `calculateRatingsAverageAndRatingsQuantity` to the `reviewSchema` to calculate and update the average rating and quantity for a product.
    - Implemented `post` middleware for `save` and `deleteOne` operations to automatically update product ratings.
    - Refactored the `reviewValidators` to use consistent naming conventions and improved error messages.
    - Ensured all Mongoose IDs are validated and checked for ownership in update and delete operations.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require( "mongoose" );
+const Product = require( "./productModel" );
 
 // 1 - Create schema
 const reviewSchema = new mongoose.Schema(
@@ -31,6 +32,60 @@ reviewSchema.pre( /^find/, function ( next ) {
     this.populate( { path: 'user', select: 'name' } );
     next();
 } );
+
+function round( num, decimals ) {
+    const n = 10 ** decimals;
+    return Math.round( ( n * num ).toFixed( decimals ) ) / n;
+};
+
+reviewSchema.statics.calculateRatingsAverageAndRatingsQuantity = async function ( productId ) {
+    const result = await this.aggregate(
+        [
+            // Stage 1 : Get all reviews about specific product
+            {
+                $match: { product: productId },
+            },
+            // Stage 2 : Grouping reviews based on productId and calculate average and quantity
+            {
+                $group: {
+                    _id: '$product',
+                    ratingsAverage: { $avg: '$rating' },
+                    ratingsQuantity: { $sum: 1 },
+                },
+            },
+        ]
+    );
+
+    console.log( result );
+
+    if ( result.length > 0 ) {
+        await Product.findByIdAndUpdate(
+            result[ 0 ]._id,
+            {
+                ratingsAverage: round( result[ 0 ].ratingsAverage, 2 ),
+                ratingsQuantity: result[ 0 ].ratingsQuantity,
+            }
+        );
+    }
+    else {
+        await Product.findByIdAndUpdate( this.product, {
+            ratingsAverage: 0,
+            ratingsQuantity: 0,
+        } );
+    }
+};
+
+reviewSchema.post( "save", async function () {
+    await this.constructor.calculateRatingsAverageAndRatingsQuantity( this.product );
+} );
+
+reviewSchema.post( "deleteOne", async function ( document ) {
+    console.log( 'deleteOne' );
+    console.log( document );
+    console.log( this.product );
+    // await this.constructor.calculateRatingsAverageAndRatingsQuantity( this.product );
+} );
+
 
 
 const reviewModel = mongoose.model( "Review", reviewSchema );
--- utils/validators/reviewValidators.js
+++ utils/validators/reviewValidators.js
@@ -1,29 +1,29 @@
-const { check, body } = require('express-validator');
-const validatorMiddleware = require('../../middlewares/validatorMiddleware');
-const Review = require('../../models/reviewModel');
+const { check, body } = require( 'express-validator' );
+const validatorMiddleware = require( '../../middlewares/validatorMiddleware' );
+const Review = require( '../../models/reviewModel' );
 
 exports.createReviewValidator = [
-  check('comment').optional(),
+  check( 'comment' ).optional(),
 
-  check('ratings')
+  check( 'rating' )
     .notEmpty()
-    .withMessage('ratings value required')
-    .isFloat({ min: 1, max: 5 })
-    .withMessage('Ratings value must be between 1 to 5'),
+    .withMessage( 'rating value required' )
+    .isFloat( { min: 1, max: 5 } )
+    .withMessage( 'Rating value must be between 1 to 5' ),
 
-  check('user').isMongoId().withMessage('Invalid Review id format'),
-  
-  check('product')
+  check( 'user' ).isMongoId().withMessage( 'Invalid Review id format' ),
+
+  check( 'product' )
     .isMongoId()
-    .withMessage('Invalid Review id format')
-    .custom((val, { req }) =>
+    .withMessage( 'Invalid Review id format' )
+    .custom( ( val, { req } ) =>
       // Check if logged user create review before
-      Review.findOne({ user: req.user._id, product: req.body.product }).then(
-        (review) => {
-          console.log(review);
-          if (review) {
+      Review.findOne( { user: req.user._id, product: req.body.product } ).then(
+        ( review ) => {
+          console.log( review );
+          if ( review ) {
             return Promise.reject(
-              new Error('You already created a review before')
+              new Error( 'You already created a review before' )
             );
           }
         }
@@ -33,52 +33,52 @@ exports.createReviewValidator = [
 ];
 
 exports.getReviewValidator = [
-  check('id').isMongoId().withMessage('Invalid Review id format'),
+  check( 'id' ).isMongoId().withMessage( 'Invalid Review id format' ),
   validatorMiddleware,
 ];
 
 exports.updateReviewValidator = [
-  check('id')
+  check( 'id' )
     .isMongoId()
-    .withMessage('Invalid Review id format')
-    .custom((val, { req }) =>
+    .withMessage( 'Invalid Review id format' )
+    .custom( ( val, { req } ) =>
       // Check review ownership before update
-      Review.findById(val).then((review) => {
-        if (!review) {
-          return Promise.reject(new Error(`There is no review with id ${val}`));
+      Review.findById( val ).then( ( review ) => {
+        if ( !review ) {
+          return Promise.reject( new Error( `There is no review with id ${ val }` ) );
         }
 
-        if (review.user._id.toString() !== req.user._id.toString()) {
+        if ( review.user._id.toString() !== req.user._id.toString() ) {
           return Promise.reject(
-            new Error(`Your are not allowed to perform this action`)
+            new Error( `Your are not allowed to perform this action` )
           );
         }
-      })
+      } )
     ),
   validatorMiddleware,
 ];
 
 exports.deleteReviewValidator = [
-  check('id')
+  check( 'id' )
     .isMongoId()
-    .withMessage('Invalid Review id format')
-    .custom((val, { req }) => {
+    .withMessage( 'Invalid Review id format' )
+    .custom( ( val, { req } ) => {
       // Check review ownership before update
-      if (req.user.role === 'user') {
-        return Review.findById(val).then((review) => {
-          if (!review) {
+      if ( req.user.role === 'user' ) {
+        return Review.findById( val ).then( ( review ) => {
+          if ( !review ) {
             return Promise.reject(
-              new Error(`There is no review with id ${val}`)
+              new Error( `There is no review with id ${ val }` )
             );
           }
-          if (review.user._id.toString() !== req.user._id.toString()) {
+          if ( review.user._id.toString() !== req.user._id.toString() ) {
             return Promise.reject(
-              new Error(`Your are not allowed to perform this action`)
+              new Error( `Your are not allowed to perform this action` )
             );
           }
-        });
+        } );
       }
       return true;
-    }),
+    } ),
   validatorMiddleware,
 ];
